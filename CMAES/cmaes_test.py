b3527acbb24ee490c95b87672e1988e38f9ee8af
Kreyparion <kreyparion@gmail.com>
2024-03-29 10:02:32 +0100

    add training for input and output

--- CMAES/cmaes_test.py
+++ CMAES/cmaes_test.py
@@ -34,7 +34,7 @@ def mnist_reduced(input_dim=4):
     x_test = reducer.transform(x_test)
     return (x_train, y_train), (x_test, y_test)
 
-def train_esn(input_size,reservoir_size=50,df_path: str = "CMAES/display_evolution.csv",):
+def train_esn_input(input_size,reservoir_size=50,df_path: str = "CMAES/display_evolution.csv",):
     (x_train, y_train), (x_test, y_test) = mnist_reduced(input_size)
     esn = ESN(
         n_inputs=input_size,
@@ -94,6 +94,68 @@ def train_esn(input_size,reservoir_size=50,df_path: str = "CMAES/display_evoluti
     esn.W_in = best_sol
     print("After all the generations, the best error is: ", best_error)
 
+def train_all_esn(input_size,reservoir_size=50,df_path: str = "CMAES/display_evolution_all.csv",):
+    (x_train, y_train), (x_test, y_test) = mnist_reduced(input_size)
+    esn = ESN(
+        n_inputs=input_size,
+        n_outputs=10,
+        spectral_radius=0.8,
+        n_reservoir=reservoir_size,
+        sparsity=0.5,
+        silent=False,
+        input_scaling=0.7,
+        feedback_scaling=0.2,
+        wash_out=25,
+        learn_method="pinv",
+        random_state=12
+    )
+    
+    upper_bound = np.ones(esn.W_in.shape[0]*esn.W_in.shape[1]+esn.W_out.shape[0]*esn.W_out.shape[1])
+    lower_bound = -np.ones(esn.W_in.shape[0]*esn.W_in.shape[1]+esn.W_out.shape[0]*esn.W_out.shape[1])
+    bounds = np.stack((lower_bound, upper_bound), axis=1)
+    optimizer = CMA(mean=np.zeros(esn.W_in.shape[0]*esn.W_in.shape[1] + esn.W_out.shape[0]*esn.W_out.shape[1]), sigma=1, bounds=bounds, population_size=50)
+    best_sol = None
+    best_error = np.inf
+    result_records: List[Dict[str, Any]] = []
+    try:
+        for generation in range(100):
+            solutions = []
+            for i in range(optimizer.population_size):
+                esn_result = optimizer.ask()
+                esn.W_in = esn_result[:esn.W_in.shape[0]*esn.W_in.shape[1]].reshape(esn.W_in.shape)
+                esn.W_out = esn_result[esn.W_in.shape[0]*esn.W_in.shape[1]:].reshape(esn.W_out.shape)
+                pred_train = esn.predict(x_train, continuation=True)
+                pred_test = esn.predict(x_test, continuation=False)
+                train_acc = accuracy(pred_train, y_train)
+                test_acc = accuracy(pred_test, y_test)
+                error = 1 - train_acc
+                if error < best_error:
+                    best_error = error
+                    best_sol = esn.W_in
+                print(error)
+                solutions.append((esn_result, error))
+                result_records.append({
+                    "generation": generation,
+                    "individual": i,
+                    "train_accuracy": train_acc,
+                    "test_accuracy": test_acc,
+                })
+                # Saving data by concatenating with what we already have and keeping the latest
+                df = pd.read_csv(df_path, sep=";")
+                df2: pd.DataFrame = pd.DataFrame.from_records(result_records)
+                df = pd.concat([df, df2])
+                df.sort_values(by=["test_accuracy"], ascending=True, inplace=True)
+                df.drop_duplicates(subset=["generation", "individual"], keep="last", inplace=True)
+                df.sort_values(by=["generation", "individual"], inplace=True)
+                df.to_csv(df_path, index=False, sep=";")
+            print("Generation: ", generation, "Best error: ", np.min([x[1] for x in solutions]))
+            optimizer.tell(solutions)
+    except KeyboardInterrupt:
+        pass
+    esn.W_in = best_sol
+    print("After all the generations, the best error is: ", best_error)
+
+
 def visualize():
     df = pd.read_csv("CMAES/display_evolution.csv", sep=";")
     df = df.groupby("generation").mean()
@@ -101,5 +163,5 @@ def visualize():
     plt.show()
 
 if __name__ == "__main__":
-    train_esn(4, 50)
+    train_all_esn(10, 50)
     # visualize()
