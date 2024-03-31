b155668fc54386dd0cbeaf2c25dcc631923ab72b
Kreyparion <kreyparion@gmail.com>
2024-03-31 19:21:54 +0200

    keep normal esn input train

--- CMAES/cmaes_test.py
+++ CMAES/cmaes_test.py
@@ -36,6 +36,63 @@ def mnist_reduced(input_dim=4):
     x_test = reducer.transform(x_test)
     return (x_train, y_train), (x_test, y_test)
 
+def train_esn_input(input_size,reservoir_size=50,df_path: str = "CMAES/display_evolution.csv",):
+    (x_train, y_train), (x_test, y_test) = mnist_reduced(input_size)
+    esn = ESN(
+        n_inputs=input_size,
+        n_outputs=10,
+        spectral_radius=0.8,
+        n_reservoir=reservoir_size,
+        sparsity=0.5,
+        silent=False,
+        input_scaling=0.7,
+        feedback_scaling=0.2,
+        wash_out=25,
+        learn_method="pinv",
+        random_state=12,
+        allow_cut_connections=False
+    )
+    
+    upper_bound = np.ones(esn.W_in.shape[0]*esn.W_in.shape[1])
+    lower_bound = -np.ones(esn.W_in.shape[0]*esn.W_in.shape[1])
+    bounds = np.stack((lower_bound, upper_bound), axis=1)
+    optimizer = CMA(mean=np.zeros(esn.W_in.shape[0]*esn.W_in.shape[1]), sigma=1, bounds=bounds, population_size=200)
+    best_sol = None
+    best_error = np.inf
+    try:
+        for generation in range(500):
+            solutions = []
+            for i in range(optimizer.population_size):
+                esn_result = optimizer.ask()
+                esn.W_in = esn_result.reshape(esn.W_in.shape)
+                _ = esn.fit(x_train, y_train)
+                pred_train = esn.predict(x_train, continuation=True)
+                pred_test = esn.predict(x_test, continuation=False)
+                train_acc = accuracy(pred_train, y_train)
+                test_acc = accuracy(pred_test, y_test)
+                error = 1 - train_acc
+                if error < best_error:
+                    best_error = error
+                    best_sol = esn.W_in
+                print(error)
+                solutions.append((esn_result, error))
+                data_for_csv = {
+                    "generation": generation,
+                    "individual": i,
+                    "train_accuracy": train_acc,
+                    "test_accuracy": test_acc,
+                }
+                df = pd.DataFrame(data_for_csv, index=[0])
+                df.to_csv(df_path, mode="a", header=False, sep=";", index=False)          
+            print("Generation: ", generation, "Best error: ", np.min([x[1] for x in solutions]))
+            optimizer.tell(solutions)
+            save_pickle(optimizer, "CMAES/optimizer_input.pickle")
+            save_pickle(esn, "CMAES/esn_input.pickle")
+    except KeyboardInterrupt:
+        pass
+    esn.W_in = best_sol
+    print("After all the generations, the best error is: ", best_error)
+
 def train_esn_input_torch(input_size,reservoir_size=50,df_path: str = "CMAES/display_evolution.csv",):
     (x_train, y_train), (x_test, y_test) = mnist_reduced(input_size)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
